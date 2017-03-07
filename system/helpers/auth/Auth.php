18c95e304a09a0b2d5235d55ba8fb5a5ea57fa97
Dılo abinin yeri <dilsizkaval@windowslive.com>
2017-03-07 14:13:50 +0200

    Update and rename loginAuth.php to Auth.php

---
+++ system/helpers/auth/Auth.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kutuphane
+ * Date: 10.12.2016
+ * Time: 18:13
+ */
+
+nmaespace System\helpers\auth;
+class Auth
+{
+
+    public function check(){
+
+
+    }
+    
+    
+    public function logout(){
+        
+        
+    }
+}
