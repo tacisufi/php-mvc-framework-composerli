7844e36d77f3c372f39c3baec4277705376b0d1a
Dılo abinin yeri <dilsizkaval@windowslive.com>
2017-03-17 15:44:32 +0200

    Update console.php
    for create controller you can it code
    php console.php -c controllername  
    
    for  create model you can it code
    php console.php -m modelsname

--- console.php
+++ console.php
@@ -1,9 +1,73 @@
 #!/bin/usr/php
 
+
+#!/usr/bin/php
 <?php
 
 
-var_dump($argv);
 
+if($argv[1]=="-c"){
+
+	$a=$argv[2];
+
+$class="<?php
+
+namespace App\controllers;
+
+
+
+
+class $a{
+
+
+
+}";
+
+$dt = fopen('app/controllers/'.$argv[2].'.php', 'w');
+
+fwrite($dt, $class);
+fclose($dt);
+echo "app/controllers/".$argv[2]."created succesfully";
+
+
+}
+else if($argv[1]=="-m"){
+
+
+
+	$a=$argv[2];
+
+$class="<?php
+
+namespace App\models;
+use System\helpers\general\craud;
+
+
+
+
+
+class $a{
+
+	
+
+use craud;
+
+protected $"."table"."='yourtable'; 
+
+
+
+}";
+
+$dt = fopen('app/Models/'.$argv[2].'.php', 'w');
+
+fwrite($dt, $class);
+fclose($dt);
+echo "app/models/".$argv[2]."created succesfully";
+
+}
+else {
+
+	echo "The command is wrong";
+}
 
 ?>
